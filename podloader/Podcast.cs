a4d542bbf682079ac121033f8def8a942a6b3494
Andrew Kluthe <andrew@ctech.me>
2023-09-12 18:07:52 -0500

    tweaking a few things trying to do a feed

---
+++ podloader/Podcast.cs
@@ -0,0 +1,54 @@
+﻿namespace podloader
+{
+    /* 
+  Licensed under the Apache License, Version 2.0
+
+  http://www.apache.org/licenses/LICENSE-2.0
+  */
+
+    using System.Xml.Serialization;
+
+
+    [XmlRoot(ElementName = "enclosure")]
+    public class Enclosure
+    {
+        [XmlAttribute(AttributeName = "url")]
+        public string Url { get; set; }
+        [XmlAttribute(AttributeName = "type")]
+        public string Type { get; set; }
+    }
+
+    [XmlRoot(ElementName = "item")]
+    public class Item
+    {
+        [XmlElement(ElementName = "title")]
+        public string Title { get; set; }
+        [XmlElement(ElementName = "description")]
+        public string Description { get; set; }
+        [XmlElement(ElementName = "enclosure")]
+        public Enclosure Enclosure { get; set; }
+    }
+
+    [XmlRoot(ElementName = "channel")]
+    public class Channel
+    {
+        [XmlElement(ElementName = "title")]
+        public string Title { get; set; }
+        [XmlElement(ElementName = "description")]
+        public string Description { get; set; }
+        [XmlElement(ElementName = "category")]
+        public string Category { get; set; }
+        [XmlElement(ElementName = "item")]
+        public List<Item> Item { get; set; }
+    }
+
+    [XmlRoot(ElementName = "rss")]
+    public class Rss
+    {
+        [XmlElement(ElementName = "channel")]
+        public Channel Channel { get; set; }
+        [XmlAttribute(AttributeName = "version")]
+        public string Version { get; set; }
+    }
+
+}
--- podloader/Program.cs
+++ podloader/Program.cs
@@ -2,8 +2,10 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using podloader;
 using podloader.Services.KdhxHostedService;
 using System.Text.Json;
+using System.Xml.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -29,10 +31,54 @@ app.MapGet("/status", ([FromServices] KdhxHostedService service, [FromServices] 
     return JsonSerializer.Serialize(result);
 });
 
+
+string serverIPAddress = "192.168.1.20";
+int serverPort = 9876;
+var feedFullPath = "/Temp/webroot/feed.rss";
+var audioRootUrl = $"http://{serverIPAddress}:{serverPort}";
+
 // Your existing podcast logic
 app.MapGet("/podcast/kdhx", async () =>
 {
-    // Your logic here
+    // return podcast rss feed for path.combine("kdhxfiles", "*.mp3"), use a nuget to create the podcast response
+
+    var directory = Path.Combine("kdhxfiles");
+    var files = Directory.GetFiles(directory, "*.mp3");
+
+    var rss = new Rss
+    {
+        Version = "2.0",
+        Channel = new Channel
+        {
+            Title = "[Local] Test Podcast",
+            Description = "Testing generating RSS feed from local MP3 files",
+            Category = "test",
+            Item = new List<Item>()
+        }
+    };
+    var allMp3s = new DirectoryInfo(directory)
+        .GetFiles("*.mp3", SearchOption.AllDirectories)
+        .OrderBy(x => x.Name);
+
+    foreach (var mp3 in allMp3s)
+    {
+        rss.Channel.Item.Add(new Item
+        {
+            Description = mp3.Name,
+            Title = mp3.Name,
+            Enclosure = new Enclosure
+            {
+                Url = $"{audioRootUrl}/{mp3.Name}",
+                Type = "audio/mpeg"
+            }
+        });
+    }
+    var serializer = new XmlSerializer(typeof(Rss));
+    using (var writer = new StreamWriter(feedFullPath))
+    {
+        serializer.Serialize(writer, rss);
+    }
+
 });
 
 app.Run();
